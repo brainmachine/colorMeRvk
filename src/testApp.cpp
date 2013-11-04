1968398ac849a9e0f49e92d9189a0290a7559dde
brainmachine <leostefansson@gmail.com>
2013-11-04 11:57:34 -0800

    funky mouseY

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -13,8 +13,8 @@ void testApp::update(){
 //--------------------------------------------------------------
 void testApp::draw(){
     for (int i = 0; i < ofGetWidth(); i++) {
-        ofSetColor(i/4, i/3, i/2);
-        ofLine(i, 0, i*sin(i)*100, i*100);
+        ofSetColor(i/1, i/3, i/2);
+        ofLine(i, 0, i*sin(i)*100, i*mouseY);
     }
 }
 
